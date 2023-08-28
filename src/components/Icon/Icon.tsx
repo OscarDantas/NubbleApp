9ad200d6e4406efbf6dd7aaef8c8bef85e1e1873
Oscar Tenorio <oscartenorio@MacBook-Air-de-Oscar.local>
2023-08-27 22:39:17 -0300

    update all imports

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -28,9 +28,10 @@ import {ProfileIcon} from '../../assets/icons/ProfileIcon';
 import {SearchIcon} from '../../assets/icons/SearchIcon';
 import {SettingsIcon} from '../../assets/icons/SettingsIcon';
 import {TrashIcon} from '../../assets/icons/TrashIcon';
-import {useAppTheme} from '../../hooks/useAppTheme';
-import {ThemeColors} from '../../theme/theme';
 import {Pressable} from 'react-native';
+
+import {useAppTheme} from '@hooks';
+import {ThemeColors} from '@theme';
 
 export interface IconBase {
   size?: number;
