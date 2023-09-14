35fd394118743ae8c2529d1962b1732393bf6f54
Oscar Tenorio <oscartenorio@MacBook-Air-de-Oscar.local>
2023-09-13 22:46:15 -0300

    typescript nested navigation

--- src/screens/app/NewPostScreen/NewPostScreen.tsx
+++ src/screens/app/NewPostScreen/NewPostScreen.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
 import {Screen, Text} from '@components';
+import {AppTabScreenProps} from '@routes';
 
-export function MyProfileScreen() {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function NewPostScreen(props: AppTabScreenProps<'NewPostScreen'>) {
   return (
     <Screen>
       <Text preset="headingSmall">My Profile Screen</Text>
