1c2a52068c0fbc6ef19d680c8dd6ace0036b88c6
Oscar Tenorio <oscartenorio@MacBook-Air-de-Oscar.local>
2023-09-23 00:28:19 -0300

    1 - setup backend

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -4,6 +4,6 @@ export const api = axios.create({
   baseURL: 'http://127.0.0.1:3333/',
   headers: {
     Authorization:
-      'Bearer NA.dEs0-h0BDOJvtQSFeiK5BbRvyMbT-QybP-6r0n5zco5AaVMDQF3DO27eN3ii',
+      'Bearer MQ.xJMii2zqUwUJd5iNp1UowxjPChqP_rOfSM6dEAAey9R8nnhjxoJaiTTb2XBo',
   },
 });
