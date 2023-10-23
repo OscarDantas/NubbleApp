61a3c3f0a195e1f89bd259226a0c4d14d50a3cb6
Oscar Tenorio <oscartenorio@MacBook-Air-de-Oscar.local>
2023-10-22 23:57:35 -0300

    11 - TextMessage

--- src/screens/app/PostCommentScreen/PostCommentScreen.tsx
+++ src/screens/app/PostCommentScreen/PostCommentScreen.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {FlatList, ListRenderItemInfo} from 'react-native';
 
 import {PostComment, usePostCommentList} from '@domain';
 
-import {Screen} from '@components';
+import {Box, Screen, TextMessage} from '@components';
 import {useAppSafeArea} from '@hooks';
 import {AppScreenProps} from '@routes';
 
 import {PostCommentBottom, PostCommentItem} from './components';
-
 export function PostCommentScreen({
   route,
 }: AppScreenProps<'PostCommentScreen'>) {
   const postId = route.params.postId;
   const {list, fetchNextPage, hasNextPage} = usePostCommentList(postId);
+  const [message, setMessage] = useState('');
 
   const {bottom} = useAppSafeArea();
 
@@ -21,20 +21,30 @@ export function PostCommentScreen({
     return <PostCommentItem postComment={item} />;
   }
 
+  function onPressSend() {}
+
   return (
-    <Screen title="Comentários" canGoBack>
-      <FlatList
-        showsVerticalScrollIndicator={false}
-        data={list}
-        renderItem={renderItem}
-        contentContainerStyle={{paddingBottom: bottom}}
-        ListFooterComponent={
-          <PostCommentBottom
-            hasNextPage={hasNextPage}
-            fetchNextPage={fetchNextPage}
-          />
-        }
-      />
+    <Screen flex={1} title="Comentários" canGoBack>
+      <Box flex={1} justifyContent="space-between">
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          data={list}
+          renderItem={renderItem}
+          contentContainerStyle={{paddingBottom: bottom}}
+          ListFooterComponent={
+            <PostCommentBottom
+              hasNextPage={hasNextPage}
+              fetchNextPage={fetchNextPage}
+            />
+          }
+        />
+        <TextMessage
+          placeholder="Adicione um comentário"
+          onPressSend={onPressSend}
+          value={message}
+          onChangeText={setMessage}
+        />
+      </Box>
     </Screen>
   );
 }
