1a7ea6a8ad077a6ad6cec25e520b20cda4110bdd
Oscar Tenorio <oscartenorio@MacBook-Air-de-Oscar.local>
2023-10-29 23:33:41 -0300

    2 - toast service (context)

--- App.tsx
+++ App.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
+import {ToastProvider} from '@services';
 import {ThemeProvider} from '@shopify/restyle';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+
+import {Toast} from '@components';
 
 import {Router} from './src/routes/Routes';
 import {theme} from './src/theme/theme';
@@ -10,10 +13,12 @@ function App(): JSX.Element {
   return (
     <SafeAreaProvider>
       <ThemeProvider theme={theme}>
-        <Router />
+        <ToastProvider>
+          <Router />
+          <Toast />
+        </ToastProvider>
       </ThemeProvider>
     </SafeAreaProvider>
   );
 }
-
 export default App;
