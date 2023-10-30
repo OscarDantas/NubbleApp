bc156bd183296cb35ce4ea21fac87361e1143745
Oscar Tenorio <oscartenorio@MacBook-Air-de-Oscar.local>
2023-10-30 00:00:48 -0300

    5 - style

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -1,47 +1,27 @@
 import {useEffect} from 'react';
-import {Dimensions} from 'react-native';
 
 import {useToast, useToastService} from '@services';
 
-import {$shadowProps} from '@theme';
+import {ToasContent} from './components/ToasContent';
 
-import {Box, BoxProps} from '../Box/Box';
-import {Icon} from '../Icon/Icon';
-import {Text} from '../Text/Text';
-const MAX_WIDTH = Dimensions.get('screen').width * 0.9;
+const DEFAULT_DURATION = 2000;
 
 export function Toast() {
   const toast = useToast();
+
   const {hideToast} = useToastService();
 
   useEffect(() => {
     if (toast) {
       setTimeout(() => {
         hideToast();
-      }, 2000);
+      }, toast.duration || DEFAULT_DURATION);
     }
   }, [hideToast, toast]);
+
   if (!toast) {
     return null;
   }
-  return (
-    <Box top={100} {...$boxStyle}>
-      <Icon color="success" name="checkRound" />
-      <Text style={{flexShrink: 1}} ml="s16" preset="paragraphMedium" bold>
-        {toast?.message}
-      </Text>
-    </Box>
-  );
+
+  return <ToasContent toast={toast} />;
 }
-const $boxStyle: BoxProps = {
-  position: 'absolute',
-  backgroundColor: 'background',
-  alignSelf: 'center',
-  alignItems: 'center',
-  padding: 's16',
-  borderRadius: 's16',
-  flexDirection: 'row',
-  opacity: 0.95,
-  maxWidth: MAX_WIDTH,
-  style: {...$shadowProps},
-};
