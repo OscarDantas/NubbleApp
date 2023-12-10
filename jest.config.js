247fa41f339670495c15bed91642858eb67ea1ab
Oscar Tenorio <oscartenorio@MacBook-Air-de-Oscar.local>
2023-12-09 22:27:00 -0300

    14 - jest setupFiles

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'react-native',
+  setupFiles: ['<rootDir>/src/test/jestSetup.ts'],
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   moduleDirectories: ['node_modules', './src/test'],
   modulePathIgnorePatterns: ['.*/mockedData/.*'],
